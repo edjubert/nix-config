bdd7f0e0baac05198429cecec5f9eeb3a0637322
edjubert <edouard.jubert@gmail.com>
2023-08-14 11:13:12 +0200

    deleted duplicated configs

--- home-manager/rofi/launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rofi -modi drun -show drun -line-padding 4 \
-  -columns 2 -paddin 50 -hide-scrollbar -terminal xfce4-terminal \
-  -show-icons -drun-icon-theme "Arch-X-D" -font "Droid Sans Regular 10"
